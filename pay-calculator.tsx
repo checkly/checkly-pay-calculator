4a8c448795068335a633e6db6ab131f2d4b420d5
Kaylie Boogaerts <kaylie@checklyhq.com>
2026-04-23 09:40:54 +0200

    currency conversion

--- pay-calculator.tsx
+++ pay-calculator.tsx
@@ -76,9 +76,9 @@ const salaries = [
   { role: "SDR", seniority: "Junior", salary: "36750" },
   { role: "SDR", seniority: "Intermediate", salary: "49000" },
   { role: "SDR", seniority: "Senior", salary: "61250" },
-  { role: "Account Executive", seniority: "Junior", salary: "64228.50" },
-  { role: "Account Executive", seniority: "Intermediate", salary: "85637.99" },
-  { role: "Account Executive", seniority: "Senior", salary: "107047.49" },
+  { role: "Account Executive", seniority: "Junior", salary: "64199.74" },
+  { role: "Account Executive", seniority: "Intermediate", salary: "85599.66" },
+  { role: "Account Executive", seniority: "Senior", salary: "106999.57" },
   { role: "Customer Solutions", seniority: "Junior", salary: "63293" },
   { role: "Customer Solutions", seniority: "Intermediate", salary: "84391" },
   { role: "Customer Solutions", seniority: "Senior", salary: "105489" },
@@ -155,8 +155,8 @@ export default function PayCalculator() {
 
       setSalaryEURMin(Math.trunc(salaryInEURMin).toLocaleString())
       setSalaryEURMax(Math.trunc(salaryInEURMax).toLocaleString())
-      setSalaryUSDMin(Math.trunc(salaryInEURMin * 1.167706).toLocaleString())
-      setSalaryUSDMax(Math.trunc(salaryInEURMax * 1.167706).toLocaleString())
+      setSalaryUSDMin(Math.trunc(salaryInEURMin * 1.168229).toLocaleString())
+      setSalaryUSDMax(Math.trunc(salaryInEURMax * 1.168229).toLocaleString())
     }
   }
 
